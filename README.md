0faa08e097e41953ea43eac1d26627588c79843b
Peter Hull <petermnhull@gmail.com>
2024-03-23 09:18:21 +0000

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # nvim-dotenv
-Tiny Neovim plugin for setting env vars
+
+Tiny Neovim plugin for setting env vars from a `.env` file in the root directory of the project you're working on.
+
+Run `<Space>ts` to set the environment variables.
+
+## Installation
+
+For Lazy:
+
+```
+return {
+  'petermnhull/nvim-dotenv.nvim',
+  opts = {}
+}
+```
+
+## TODO
+
+Some ideas to make this into a proper plugin.
+
+- Let client set key mapping instead.
+- Option for specifying which `.env` file to use.
+- Option for running the function on start up.
---
+++ lua/nvim-dotenv/init.lua
@@ -0,0 +1,61 @@
+local M = {}
+
+local function get_lines_from_file(str)
+	local lines = {}
+
+	local function insert_line(line)
+		table.insert(lines, line)
+		return ""
+	end
+
+	insert_line((str:gsub("(.-)\r?\n", insert_line)))
+
+	return lines
+end
+
+local function split_string(str, separator)
+	if separator == nil then
+		separator = "%s"
+	end
+
+	local t = {}
+	for s in string.gmatch(str, "([^" .. separator .. "]+)") do
+		table.insert(t, s)
+	end
+
+	return t
+end
+
+local function set_env_from_file()
+	local file_path = "test.env"
+	local file, _ = io.open(file_path, "r")
+	if file ~= nil then
+		local content = file:read("*a")
+		local lines = get_lines_from_file(content)
+		if next(lines) ~= nil then
+			for v in pairs(lines) do
+				local line = lines[v]
+				if not (line == nil or line == "") and string.sub(line, 1, 1) ~= "#" then
+					local line_values = split_string(line, "=")
+					if line_values ~= nil then
+						local value = line_values[2] and vim.trim(line_values[2]) or ""
+						vim.env[line_values[1]] = value
+					end
+				end
+			end
+		end
+		file:close()
+	end
+	print("loaded env vars from test.env")
+end
+
+M.setup = function(opts)
+	vim.keymap.set(
+		"n",
+		"<leader>ts",
+		set_env_from_file,
+		{ noremap = true, silent = true, desc = "[s]et env from test.env file" }
+	)
+end
+
+return M
